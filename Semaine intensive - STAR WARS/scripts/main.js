7ff7c2ea369e2fab7e2957595a7567f1a44f1e44
Anthelme-RISSER <anthelme.risser@gmail.com>
2015-12-14 15:57:38 +0100

    background star converti en js
    blablabla

--- Semaine intensive - STAR WARS/scripts/main.js
+++ Semaine intensive - STAR WARS/scripts/main.js
@@ -1,1 +1,29 @@
 console.log('yo');
+
+function Create_Star(){
+    
+    var cluster = document.querySelector('.cluster');
+    
+    var star = '<div class="star"></div>';
+    for(var i = 0; i < 800; i++){
+        
+        star = star + ' <div class="star"></div>'
+    }
+    
+    cluster.innerHTML = star;
+    
+    var star__style = "<style>";
+    
+    for(var i = 0; i < 800; i++){
+        var x = parseInt(Math.random() * 4000 - 2000);
+        var y = parseInt(Math.random() * 4000 - 2000);
+        var z = parseInt(Math.random() * 4000 - 2000);
+        star__style = star__style + " .star:nth-child(" + i + ") {-webkit-transform: translate3d(1455px, -128px, -15px);transform: translate3d(" + x + "px, " + y + "px, " + z + "px);} ";
+    }
+    star__style += "</style>";
+    document.write(star__style);
+    
+    
+}
+
+Create_Star();
