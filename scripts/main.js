7de513a8b47944e60f2863d404f84afa0f912b44
qrek <theobacholier@MacBook-Pro-de-Theo.local>
2015-12-14 23:17:48 +0100

    Add Vue.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,56 @@
 console.log('yo');
 
-function Create_Star(){
+// function Create_Star(){
     
-    var cluster = document.querySelector('.cluster');
+//     var cluster = document.querySelector('.cluster');
     
-    var star = '<div class="star"></div>';
-    for(var i = 0; i < 800; i++){
+//     var star = '<div class="star"></div>';
+//     for(var i = 0; i < 800; i++){
         
-        star = star + ' <div class="star"></div>'
-    }
+//         star = star + ' <div class="star"></div>'
+//     }
     
-    cluster.innerHTML = star;
+//     cluster.innerHTML = star;
     
-    var star__style = "<style>";
+//     var star__style = "<style>";
     
-    for(var i = 0; i < 800; i++){
-        var x = parseInt(Math.random() * 4000 - 2000);
-        var y = parseInt(Math.random() * 4000 - 2000);
-        var z = parseInt(Math.random() * 4000 - 2000);
-        star__style = star__style + " .star:nth-child(" + i + ") {-webkit-transform: translate3d(1455px, -128px, -15px);transform: translate3d(" + x + "px, " + y + "px, " + z + "px);} ";
-    }
-    star__style += "</style>";
-    document.write(star__style);
+//     for(var i = 0; i < 800; i++){
+//         var x = parseInt(Math.random() * 4000 - 2000);
+//         var y = parseInt(Math.random() * 4000 - 2000);
+//         var z = parseInt(Math.random() * 4000 - 2000);
+//         star__style = star__style + " .star:nth-child(" + i + ") {-webkit-transform: translate3d(1455px, -128px, -15px);transform: translate3d(" + x + "px, " + y + "px, " + z + "px);} ";
+//     }
+//     star__style += "</style>";
+//     document.write(star__style);
     
     
-}
+// }
 
-Create_Star();
+// Create_Star();
+
+
+var vue = new Vue({
+  el: '#app',
+  data: {
+    model: ''
+  }
+})
+
+
+var request = new XMLHttpRequest();
+request.open('GET', 'http://swapi.co/api/starships/9', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    var data = JSON.parse(request.responseText);
+  } else {
+    console.log("Error")
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
